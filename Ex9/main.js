f841016e27610108d610152ec5997c600f0271d7
R0454602 <r0454602@student.kuleuven.be>
2017-01-07 20:01:57 +0100

    mongo + first

--- Ex9/main.js
+++ Ex9/main.js
@@ -2,9 +2,12 @@
 // inladen van de dependencies
 var express = require('express'); // eenvoudige webserver in node js
 var parser = require('body-parser'); // extensie op express voor eenvoudig body uit te lezen
+var mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost:27017/MijnAPI');
 
 // onze lokale 'datastore'. deze variable bewaart onze state.
-var dal = require("./storage.js");
+//var dal = require("./storage.js");
+var dalLocatie = require("./locatie.js");
 
 // aanmaken van de webserver variabele
 var app = express();
